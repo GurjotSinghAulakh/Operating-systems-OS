f60486eac8ced9227e56b651448aff72c7cce11a
gurjotsinghaulakh <s351873@oslomet.no>
2022-12-25 16:39:01 +0100

    Create sum.c

---
+++ C/sum.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * This function sums the numbers from 0 to 3 and returns the result.
+ * 
+ * @return The sum of the numbers 0, 1, 2, and 3.
+ */
+int sum()
+{
+    int S=0, i;
+    for (i=0;i<4;i++)
+    {
+        S = S + i
+    }
+    return (S);
+}
+
+/**
+ * The function sum() is defined to return an integer value
+ */
+
+int main()
+{
+    int sum;
+    Sum = sum();
+    printf("Sum = %d \n", Sum);
+}
